34e150bef87e774d489a470b6844d5c56678e080
Brian T. Keller <btkeller@missouri.edu>
2024-10-20 21:00:01 +0000

    version 1.0.9

--- R/mechanisms.R
+++ R/mechanisms.R
@@ -82,13 +82,13 @@ MAR <- function(mis.rate, cause, r2, lower = TRUE) {
            c(x = 'The variable "{cause}" is not in the data set.')
         )
         p <- data |> attr('parameters')
-        if (length(which(cause %in% names(p$mean_X))) != 0) {
-            ind <- which(cause %in% names(p$mean_X))
+        if (length(which(names(p$mean_X) %in% cause)) != 0) {
+            ind <- which(names(p$mean_X) %in% cause)
             mu <- p$mean_X[ind]
             s <- sqrt((p$phi_w[ind,ind] + p$phi_b[ind,ind]))
         }
-        else if (length(which(cause %in% names(p$mean_Z))) != 0) {
-            ind <- which(cause %in% names(p$mean_Z))
+        else if (length(which(names(p$mean_Z) %in% cause)) != 0) {
+            ind <- which(names(p$mean_Z) %in% cause)
             mu <- p$mean_Z[ind]
             s <- sqrt(p$phi_b[ind + length(p$mean_X), ind + length(p$mean_X)])
         }
@@ -97,7 +97,7 @@ MAR <- function(mis.rate, cause, r2, lower = TRUE) {
         )
         val <- (data[,cause] - mu) / s
         phi <- sqrt(r2) / sqrt(1 - r2)
-        M.star <- val*phi + rnorm(length(cause))
+        M.star <- val*phi + rnorm(NROW(data))
         M <- M.star < qnorm(mis.rate, sd = sqrt(phi^2 + 1), lower.tail = lower)
         if (!lower) M <- !M
         data[M, 2] <- NA
